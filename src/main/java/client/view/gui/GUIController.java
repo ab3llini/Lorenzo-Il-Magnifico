8c6b73c44004eb5b32cbbb6877f460098c802180
Alberto Mario Bellini <albertomario.bellini@mail.polimi.it>
2017-06-27 21:28:26 +0200

    GUI Enhancements

--- src/main/java/client/view/gui/GUIController.java
+++ src/main/java/client/view/gui/GUIController.java
@@ -1,13 +1,28 @@
 package client.view.gui;
 
-import client.controller.network.Client;
-import client.view.cli.LocalMatchController;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+
+import client.controller.network.Client;
+import client.controller.network.ClientObserver;
+import client.controller.network.RemotePlayerObserver;
+import client.view.cli.LocalMatchController;
+import javafx.scene.input.MouseEvent;
+import netobject.action.Action;
+import netobject.action.immediate.ImmediateActionType;
+import netobject.notification.LobbyNotification;
+import netobject.notification.MatchNotification;
+import server.model.Match;
+import server.model.board.BonusTile;
+import server.model.board.Player;
 import server.model.board.TowerSlot;
+import server.model.card.Deck;
 import server.model.card.developement.DvptCard;
 import server.model.card.developement.DvptCardType;
+import server.model.card.leader.LeaderCard;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,10 +31,52 @@ import java.util.HashMap;
  * @author  ab3llini
  * @since   20/06/17.
  */
-public class GUIController extends NavigationController {
+public class GUIController extends NavigationController implements ClientObserver, RemotePlayerObserver{
+
+    @FXML
+    private GridPane dvptCardGrid;
+
+    @FXML
+    private ImageView territory3;
+
+    @FXML
+    private Label victoryValueTextField;
+
+    @FXML
+    private Label faithValueTextField;
+
+    @FXML
+    private Label militaryValueTextField;
+
+    @FXML
+    private Label woodValueTextField;
+
+    @FXML
+    private Label stonesValueTextField;
+
+    @FXML
+    private Label coinsValueTextField;
+
+    @FXML
+    private Label servantsValueTextField;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    void onDvptCardClick(MouseEvent event) {
+
+
+
+    }
 
     //The reference to the local match controller
     private LocalMatchController localMatchController;
+
+    HashMap<DvptCardType, HashMap<ArrayList<DvptCard>, ImageView>> dvptCardCache = new HashMap<>();
+
 
     //The local client instance
     private Client client;
@@ -32,13 +89,17 @@ public class GUIController extends NavigationController {
 
     private void loadDvptCards() {
 
-        HashMap<DvptCardType, ArrayList<DvptCard>> dvptCardCache = new HashMap<>();
 
-        dvptCardCache.put(DvptCardType.territory, this.getDvptCardsFromTower(this.localMatchController.getMatch().getBoard().getTerritoryTower()));
-        dvptCardCache.put(DvptCardType.character, this.getDvptCardsFromTower(this.localMatchController.getMatch().getBoard().getCharacterTower()));
-        dvptCardCache.put(DvptCardType.building, this.getDvptCardsFromTower(this.localMatchController.getMatch().getBoard().getBuildingTower()));
-        dvptCardCache.put(DvptCardType.venture, this.getDvptCardsFromTower(this.localMatchController.getMatch().getBoard().getVentureTower()));
 
+    }
+
+    private Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
+        for (Node node : gridPane.getChildren()) {
+            if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
+                return node;
+            }
+        }
+        return null;
     }
 
     private ArrayList<DvptCard> getDvptCardsFromTower(ArrayList<TowerSlot> tower) {
@@ -63,24 +124,74 @@ public class GUIController extends NavigationController {
 
     }
 
-    /**
-     * FXML Implementation below
-     */
 
-
-    @FXML
-    public void initialize() {
+    @Override
+    public void onDisconnection(Client client) {
 
     }
 
-    @FXML
-    private GridPane dvptCardGrid;
-
-    @FXML
-    void onDvptCardClick(MouseEvent event) {
-
+    @Override
+    public void onLoginFailed(Client client, String reason) {
 
     }
 
+    @Override
+    public void onLoginSuccess(Client client) {
 
+    }
+
+    @Override
+    public void onLobbyNotification(Client client, LobbyNotification not) {
+
+    }
+
+    @Override
+    public void onNotification(Client sender, MatchNotification notification) {
+
+    }
+
+    @Override
+    public void onModelUpdate(Client sender, Match model) {
+
+    }
+
+    @Override
+    public void onTurnEnabled(Client sender, Player player, String message) {
+
+    }
+
+    @Override
+    public void onImmediateActionAvailable(Client sender, ImmediateActionType actionType, Player player, String message) {
+
+    }
+
+    @Override
+    public void onTurnDisabled(Client sender, Player player, String message) {
+
+    }
+
+    @Override
+    public void onTimeoutExpired(Client sender, Player player, String message) {
+
+    }
+
+    @Override
+    public void onActionRefused(Client sender, Action action, String message) {
+
+    }
+
+    @Override
+    public void onActionPerformed(Client sender, Player player, Action action, String message) {
+
+    }
+
+    @Override
+    public void onLeaderCardDraftRequest(Client sender, Deck<LeaderCard> cards, String message) {
+
+    }
+
+    @Override
+    public void onBonusTileDraftRequest(Client sender, ArrayList<BonusTile> tiles, String message) {
+
+    }
 }
